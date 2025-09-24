67c514563c5bc2807fe2cb0b49121ebad25b4d7e
Ek <sgebm6@umsystem.edu>
2025-09-24 14:48:04 -0500

    Resume at: Stopping Apples If They Fall Too Far

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float changeDirChance = 0.1f;
+    public float appleDropDelay = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", appleDropDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+        //else if (Random.value < changeDirChance)
+        //{
+        //    speed *= -1;
+        //}
+    }
+
+    void FixedUpdate()
+    {
+        if (Random.value < changeDirChance)
+        {
+            speed *= -1;
+        }
+    }
+}
